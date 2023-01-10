41754d027f3b17ea71d18e3a117b4311d75faf30
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2023-01-10 14:16:08 -0800

    initial smart speaker frontend, hits localhost backend

---
+++ script.js
@@ -0,0 +1,25 @@
+// // script.js
+// const form = document.getElementById("form");
+
+// form.addEventListener("submit", submitForm);
+
+// function submitForm(e) {
+//     e.preventDefault();
+//     const name = document.getElementById("name");
+//     const file = document.getElementById("file");
+//     const formData = new FormData();
+//     formData.append("name", name.value);
+//     console.log(file)
+//     formData.append("filename", file.files[0]);
+    
+//     // delete options.headers['Content-Type'];
+//     fetch("http://localhost:5000/", {
+//         method: 'POST',
+//         body: formData
+//         // headers: {
+//         //   "Content-Type": "multipart/form-data"
+//         // }
+//     })
+//         .then((res) => console.log(res))
+//         .catch((err) => ("Error occured", err));
+// }
